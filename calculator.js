d88f23ed0f1be4f9fb2b4da250fa1ecc5f6a6a1e
Aizaz <aizaz4699@gmail.com>
2024-03-22 16:49:12 +0500

    Tweak the text

--- calculator.js
+++ calculator.js
@@ -1,5 +1,7 @@
+#! usr/bin/env node           // it tells that this file is terminal executable not only on local system but other people 
+//can also execute this code their CLI using npm tool(this line is called Shabang)
 import inquirer from "inquirer";
-const answer = await inquirer.prompt([{ message: "Enter First Number", type: "number", name: "firstNumber" }, { message: "Enter Second Number", type: "number", name: "SecondNumber" },
+const answer = await inquirer.prompt([{ message: "Enter your First Number", type: "number", name: "firstNumber" }, { message: "Enter your Second Number", type: "number", name: "SecondNumber" },
     { message: "Select one of the operator to perform action", type: "list", name: "operator", choices: ["Addition", "Subtraction", "Division", "Multiplication"] }]); //await means  wait  for input 
 console.log(answer);
 if (answer.operator === "Addition") {
